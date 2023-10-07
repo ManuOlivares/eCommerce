b68edd42743089532cbe713ee0d3648db47369ed
Manu Olivares <manu@Manus-MacBook-Pro.local>
2023-10-07 12:41:17 -0300

    Trabajando en el front

---
+++ frontend/vistas/plantilla.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    <meta name="title" content="Tienda Virtual" >
+    <meta name="description" content="Lorem ipsum es simplemente el texto de relleno de 
+    las imprentas y archivos de texto. Lorem Ipsum " >
+    <meta name="keyword" content="Lorem ipsum, es simplemente, el texto, de relleno" >
+
+    <title>Tienda Virtual</title>
+    <link rel="stylesheet" href="vistas/css/plugins/bootstrap.min.css">
+    <link rel="stylesheet" href="vistas/css/plugins/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:ital,wght@0,400;0,500;1,500&display=swap" rel="stylesheet">  
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Condensed&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="vistas/css/plantilla.css">
+    <link rel="stylesheet" href="vistas/css/cabezote.css">
+    <script src="vistas/js/plugins/jquery.min.js" ></script>
+    <script src="vistas/js/plugins/bootstrap.min.js" ></script>
+
+</head>
+<body>
+<?php
+include "modulos/cabezote.php";
+
+?>
+
+    
+
+</body>
+</html>
